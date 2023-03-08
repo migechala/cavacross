56e46ae9bf0e94dddd3760f4fdfaa9b7f5f902d3
Mikhail Chalakov <migecha@chalakov.net>
2023-03-07 23:41:30 -0800

    get user current classes and id

--- canvas_in_python.py
+++ canvas_in_python.py
@@ -3,18 +3,9 @@ import requests
 from config import API_KEY
 
 header = {'Authorization' : 'Bearer {}'.format(API_KEY)}
-
-#simple course
-url = 'https://overlake.instructure.com/api/v1/courses/9682'
+#get all courses
+url = 'https://overlake.instructure.com/api/v1/courses/'
 r = requests.get(url, headers=header)
-
-print(r.status_code)
-print(r.json()["name"])
-
-#simple assignments
-    
-url = 'https://overlake.instructure.com/api/v1/courses/9682/assignments'
-r = requests.get(url, headers=header)
-
-print(r.status_code)
-print(r.json()[0]['description'])
+print("Current classes")
+for i in r.json():
+    print("{} -- id: {}".format(i["name"], i['id']))
